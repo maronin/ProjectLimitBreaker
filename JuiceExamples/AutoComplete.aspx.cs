328ee800112ca76940440f2ced48073a76347879
Vincent Lee <vince.lee.lien@gmail.com>
2012-11-08 15:15:09 -0500

    Added button example. Note the inability to do this on checkboxes, will crash on compile

--- JuiceExamples/AutoComplete.aspx.cs
+++ JuiceExamples/AutoComplete.aspx.cs
@@ -9,6 +9,7 @@ public partial class Test_AutoComplete : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //This will be extremely useful for auto completing exercise name searches
         String[] hahaha = { "GS500F", "R1", "R6", "GSXR-600", "Fireblade", "CB1000R", "ZX-6R", "ZX-1R", "Ninja 250", "Ninja 1000", "SV650", "Hayabusa" };
 
         Autocomplete1.Source = hahaha;

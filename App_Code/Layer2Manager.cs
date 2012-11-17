520808cd9b1a6aca6c7e7c14cdbb0ae20b7a1653
Neil Guzman <bouz013@live.com>
2012-11-17 16:19:53 -0500

    merged both testjuice and my master to finish coding simple autocomplete. commented out actual code until database entries are done

--- App_Code/Layer2Manager.cs
+++ App_Code/Layer2Manager.cs
@@ -10,11 +10,11 @@ using System.Web;
 public class Layer2Manager
 {
     System.Web.HttpApplication _context;
-	
+
     public Layer2Manager()
-	{
+    {
         _context = System.Web.HttpContext.Current.ApplicationInstance;
-	}
+    }
 
     public List<ExerciseBase> getAllExercises()
     {
@@ -22,6 +22,19 @@ public class Layer2Manager
         {
             return context.ExerciseBases.OrderBy(o => o.id).ToList();
         }
+    }
+
+    public List<string> getExerciseNames()
+    {
+        List<string> rc = new List<string>(new string[] { "GS500F", "R1", "R6", "GSXR-600", "Fireblade", "CB1000R", "ZX-6R", "ZX-1R", "Ninja 250", "Ninja 1000", "SV650", "Hayabusa" });
+        /* actual legit code 
+         using (var context = new Layer2Container())
+         {
+             return context.ExerciseBases.Select(x => x.name).ToList();
+         }
+         * */
+
+        return rc;
     }
 
 }
--- Default.aspx
+++ Default.aspx
@@ -16,6 +16,17 @@
             DataSourceID="ObjectDataSource1" DataTextField="name" DataValueField="id">
         </asp:DropDownList>
     </div>
+    <h3>Neil's search motorbikes</h3>
+    <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+        <label for="_Default">
+            SEARCH EXERCISE:
+        </label>
+        <asp:TextBox runat="server" ID="autocomplete" ClientIDMode="Static" />
+        <juice:Autocomplete ID="Autocomplete1" runat="server" TargetControlID="autocomplete"
+            Source="ActionScript, AppleScript, Asp, BASIC, C, C++, Clojure, COBOL, ColdFusion, Erlang, Fortran, Groovy, Haskell, Java, JavaScript, Lisp, Perl, PHP, Python, Ruby, Scala, Scheme" />
+    </div>
     </form>
 </body>
 </html>

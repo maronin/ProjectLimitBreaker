a0f0523b697d92906e6336e07beb1d08e71e1a88
Alex Craig <andeic@hotmail.com>
2012-11-20 13:55:02 -0500

    added logic for no exercises in database

--- deleteExercise.aspx.cs
+++ deleteExercise.aspx.cs
@@ -9,7 +9,9 @@ public partial class deleteExercise : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (DropDownList1.Items.Count < 1) {
+            Label1.Text = "Nothing in here to see";
+        }
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
